8220e43b8b1f16a9ccda1cfa914c3dee128063b1
SORGIE-m <159481227+SORGIE-m@users.noreply.github.com>
2026-02-19 21:19:49 -0500

    guardado

--- problema04/util.ex
+++ problema04/util.ex
@@ -19,15 +19,105 @@ defmodule Util do
     end
   end
 
+
+
   def ingresar(mensaje) do
     mensaje
     |> IO.gets()
     |> String.trim()
   end
 
-  def ingresar(mensaje, :entero)
 
-  mensaje
-  |> Util.ingresar(:texto)
-  |> String.to_interger()
+
+
+  def ingresar(mensaje, :entero) do
+   try do
+   mensaje
+   |> ingresar(:texto)
+   |> String.to_integer()
+   rescue
+    ArgumentError ->
+    "Error, se espera que ingrese un número entero\n"
+   |> mostrar_error()
+   mensaje
+   |> ingresar(:entero)
+   end
+  end
+
+
+  def ingresar(mensaje, :texto) do
+mensaje
+|> IO.gets()
+|> String.trim()
+end
+
+def ingresar(mensaje, :entero) do
+mensaje
+|> Util.ingresar(:texto)
+|> String.to_integer()
+end
+
+
+
+def ingresar(mensaje, :entero) do
+try do
+mensaje
+|> ingresar(:texto)
+|> String.to_integer()
+rescue
+ArgumentError ->
+"Error, se espera que ingrese un número entero\n"
+|> mostrar_error()
+mensaje
+|> ingresar(:entero)
+end
+end
+
+
+def mostrar_error(mensaje) do
+IO.puts(:standard_error, mensaje)
+end
+
+def ingresar(mensaaje, :real) do
+  try di
+   mensaje
+   |> ingresar( :texto)
+   |> String.to_float()
+ rescue
+   ArgumentError ->
+    "Error, se espera que ingrese un numero real\n"
+    |> mostrar_error()
+
+    mensaje
+    |> ingresar( :real)
+  end
+
+
+
+  def ingresar(mensaje, :entero), do: ingresar(mensaje, &String.to_integer/1, :entero)
+  def ingresar(mensaje, :real), do: ingresar(mensaje, &String.to_float/1, :real)
+
+
+  def ingresar(mensaje, :entero) do
+    ingresar(
+      mensaje,
+      &String.to_integer/1,
+       :entero
+    )
+  end
+
+  def ingresar(mensaje, :real) do
+    ingresar(
+      mensaje,
+      &String.to_float/1,
+       :real
+    )
+  end
+
+
+
+
+
+
+
 end
